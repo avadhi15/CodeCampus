812c01e6ac2c29f94714667b219375b22139e8a7
Avadhi Verma <avadhiverma2002@gmail.com>
2025-09-20 23:23:21 +0530

    new changes

--- lms/src/components/student/Navbar.jsx
+++ lms/src/components/student/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk , UserButton ,useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
+
   const isCourseListPage =location.pathname.includes('/course-list');
 
-
+const {openSignIn} = useClerk();
+const {user} = useUser();
 
   return (
   <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36
@@ -15,12 +18,28 @@ const Navbar = () => {
       cursor-pointer' />
       <div className='hiddem md:flex items-center gap-5 text-gray-500'>
         <div>
-          <button>Become Educator</button>
-          <Link to='/my-enrollments'>My Enrollments</Link>
+          { user && <>
+            <button className='pr-4'>Become Educator</button> 
+            | <Link  className='pl-4' to='/my-enrollments'>My Enrollments
+            </Link>
+            </> 
+          }
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        { user ? <UserButton/> :
+          <button
+        onClick={()=> openSignIn()}
+        className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account
+        </button>
+        }
       </div>
-      <div></div>
+ {/* For Phone Screens */}
+      <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+      <div>
+        <button>Become Educator</button> 
+        | <Link to='/my-enrollments'>My Enrollments</Link>
+      </div>
+      <button><img src={assets.user_icon} alt="" /></button>
+      </div>
     </div>
   )
 }
